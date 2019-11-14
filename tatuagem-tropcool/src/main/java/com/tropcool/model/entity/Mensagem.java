0967bb363d558079398273d028d6f13844c2091f
GabrielUlisses <gulissessti@hotmail.com>
2019-11-14 19:42:02 -0300

    correção da classe mensagem

--- tatuagem-tropcool/src/main/java/com/tropcool/model/entity/Mensagem.java
+++ tatuagem-tropcool/src/main/java/com/tropcool/model/entity/Mensagem.java
@@ -1,23 +1,33 @@
 package com.tropcool.model.entity;
 
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Table
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode (callSuper = true)
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Mensagem extends AbstractEntity{
 	
 	@NotNull
--- tatuagem-tropcool/src/test/java/com/tropcool/model/service/MensagemTests.java
+++ tatuagem-tropcool/src/test/java/com/tropcool/model/service/MensagemTests.java
@@ -3,11 +3,13 @@ package com.tropcool.model.service;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
->>>>>>> 44bf6e108c615fd6b5dac3e44cc9754caef35467
 
 import com.tropcool.model.entity.Cliente;
 import com.tropcool.model.entity.Mensagem;
@@ -15,6 +17,7 @@ import com.tropcool.model.entity.Tatuador;
 import com.tropcool.model.repository.ClienteRepository;
 import com.tropcool.model.repository.MensagemRepository;
 import com.tropcool.model.repository.TatuadorRepository;
+
 
 public class MensagemTests extends AbstractIntegrationTests {
 	
@@ -59,47 +62,4 @@ public class MensagemTests extends AbstractIntegrationTests {
 		mensagemService.escreverMensagem(mensagem);
 		Assert.assertNotNull(mensagem.getId());
 	}
->>>>>>> 44bf6e108c615fd6b5dac3e44cc9754caef35467
-
-public class MensagemTests {
-	
-	@Autowired
-	private MensagemRepository mensagemRepository;
-	
-	@Autowired
-	private MensagemService mensagemService;
-	
-	@Autowired
-	private ClienteRepository clienteRepository;
-	
-	@Autowired
-	private ClienteService clienteService;
-	
-	@Autowired
-	private TatuadorRepository tatuadorRepository;
-	
-	@Autowired
-	private TatuadorService tatuadorService;
-	
-	/**
-	 * 
-	 * 		VOCÊ TENTOU E FALHOU MIZERAVELMENTE !!
-	 * 		SÓ DA ERRADO SE VC TENTAR :D
-	 * 		O ESFORÇO DE HOJE É O FRACAÇO DE AMANHÃ ;)
-	 * 
-	 * 
-	 */
-	
-	
-//	@Test
-//	public void cadastrarMensagem()
-//	{
-//		Mensagem mensagem = new Mensagem();
-//		mensagem.setLido(false);
-//		mensagem.setConteudo("A rua 15 é pros brabos");
-//		Cliente cliente = this.clienteRepository.findById(1001L).orElse(null);
-//		Tatuador tatuador = this.tatuadorRepository.findById(1001L).orElse(null);
-//		mensagem.setRemetente(tatuador);
-//		mensagem.setDestinatario(cliente);
-//	}
 }
