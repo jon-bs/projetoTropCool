55d341ce994d113c31f1ef1414329f8965847b95
Gabriel <gulissessti@hotmail.com>
2019-12-02 15:08:19 -0300

    Services com métodos de instegração dessenvolvidas

--- TropCool2019-FrontEnd/src/app/service/cliente.service.ts
+++ TropCool2019-FrontEnd/src/app/service/cliente.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Cliente } from '../model/cliente';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  listar(){
+    return this.http.get<Cliente[]>('http://localhost:4200/api/api/cliente/list/');
+  }
+
+  detalhar(clienteId: number){
+    return this.http.get<Cliente>('http://localhost:4200/api/api/cliente/find?id='+clienteId);
+
+  }
+
+  cadastrar(cliente: Cliente){
+    return this.http.post<Cliente>('http://localhost:4200/api/api/cliente/insert/', cliente);
+  }
+
+  editar(cliente: Cliente){
+    return this.http.post<Cliente>('http://localhost:4200/api/api/cliente/update/', cliente);
+  }
+
+  remover(clienteId: number){
+    return this.http.get('http://localhost:4200/api/api/cliente/remove?id='+clienteId);
+  }
 }
